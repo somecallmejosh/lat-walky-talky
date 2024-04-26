e073d2b243abaf3458c9cfc21dc236ee54581c87
somecallmejosh <josh@thebrileys.com>
2024-04-26 12:20:04 -0400

    Use audio tags.

--- components/RecordVoice.vue
+++ components/RecordVoice.vue
@@ -3,7 +3,8 @@
     <h1>Recording Status: {{ status }}</h1>
     <button @click="startSpeechRecognition" :disabled="isRecording">Start</button>
     <button @click="stopSpeechRecognition" :disabled="!isRecording">Stop</button>
-
+    <audio ref="openAudioElement" playsinline src="/ding.mp3"></audio>
+    <audio ref="closeAudioElement" playsinline src="/close.mp3"></audio>
     <div v-if="transcript">
     <h2>Transcript:</h2>
     <code>
@@ -17,6 +18,7 @@
   export default defineComponent ({
     data() {
       return {
+        audioPlayBackErrorMessage: 'Audio playback failed:',
         closeSound: new Audio('/close.mp3'),
         isRecording: false,
         openSound: new Audio('/ding.mp3'),
@@ -29,6 +31,9 @@
       };
     },
     methods: {
+      audioPlay() {
+        this.openSoundPlayed = true;
+      },
       initializeSpeechRecognition() {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
         this.recognition = new SpeechRecognition();
@@ -40,8 +45,11 @@
           const current = event.resultIndex;
           const result = event.results[current][0].transcript;
           if (result.toLowerCase().includes(this.wakeWord)) {
-            this.openSound.play();
-            this.openSoundPlayed = true;
+            this.$refs.openAudioElement.play().then(() => {
+              this.openSoundPlayed = true;
+            }).catch((error) => {
+              console.error(this.audioPlaybackErrorMessage, error);
+            });
             this.status = 'Listening...';
           }
 
@@ -52,7 +60,6 @@
           if (this.openSoundPlayed && event.results[current].isFinal) {
             this.transcript += result + ' ';
           }
-
         };
 
         this.recognition.onerror = event => {
@@ -62,7 +69,11 @@
         this.recognition.onend = () => {
           console.log('Speech Recognition Stopped');
           this.isRecording = false;
-          this.closeSound.play();
+          this.$refs.closeAudioElement.play().then(() => {
+              this.openSoundPlayed = false;
+            }).catch((error) => {
+              console.error(this.audioPlaybackErrorMessage, error);
+            });
           this.openSoundPlayed = false;
         };
       },
